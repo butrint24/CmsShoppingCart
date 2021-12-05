cce4590d6249c33e3f47f1cb098831f3d62acdca
Butrint <butrintsalihu24@gmail.com>
2021-12-06 00:44:49 +0100

    Details Component Added on Admin Panel

--- CmsShoppingCart/Areas/Admin/Controllers/PagesController.cs
+++ CmsShoppingCart/Areas/Admin/Controllers/PagesController.cs
@@ -18,12 +18,25 @@ namespace CmsShoppingCart.Areas.Admin.Controllers
         {
             this.context = context;
         }
+
+        //GET Request /admin/pages
         public async Task<IActionResult> Index()
         {
             IQueryable<Page> pages = from p in context.Pages orderby p.Sorting select p;
             List<Page> pagesList = await pages.ToListAsync();
 
             return View(pagesList);
+        }
+
+        //GET Request /admin/pages/details/id
+        public async Task<IActionResult> Details(int id)
+        {
+            Page page = await context.Pages.FirstOrDefaultAsync(x => x.Id == id);
+            if (page == null)
+            { 
+                return NotFound(); 
+            }
+            return View(page);
         }
     }
 }
